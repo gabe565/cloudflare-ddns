e8566e36b73171a63a091f9e5b9e67f47c1e7f31
Gabe Cook <git@gabe565.com>
2026-09-07 03:52:06 -0500

    chore(config): remove `ipinfo` from default sources

--- internal/config/config.go
+++ internal/config/config.go
@@ -39,7 +39,6 @@ func New() *Config {
 			lookup.CloudflareTLS.String(),
 			lookup.OpenDNSTLS.String(),
 			lookup.ICanHazIP.String(),
-			lookup.IPInfo.String(),
 			lookup.IPify.String(),
 			lookup.Cloudflare.String(),
 			lookup.OpenDNS.String(),
